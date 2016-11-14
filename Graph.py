6042d50a8e67c641a1e37a0864c37756b94a8dd5
Ron Matsliah <ronma1@gmail.com>
2016-11-14 09:46:23 -0800

    Graph class structure

---
+++ Graph.py
@@ -0,0 +1,11 @@
+import networkx as nx
+
+class Graph:
+    
+    # Construct graph from file using networkx
+    def __init__ (self, *args=None):
+        pass
+
+    # Parse single line from file            
+    def parse_line(line):
+        pass
