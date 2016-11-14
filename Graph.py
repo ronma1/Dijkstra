941e112eed700ef7f0d5b4d29fa44c7fb5d63ad0
Daniel Alfasi <daniela@perfectomobile.com>
2016-11-14 20:21:55 +0200

    Changes in init function

--- Graph.py
+++ Graph.py
@@ -3,9 +3,17 @@ import networkx as nx
 class Graph:
     
     # Construct graph from file using networkx
-    def __init__ (self, *args=None):
-        pass
+    def __init__ (self, *args):
+        self.graph = nx.Graph()
+        # Open the input file and create the graph from the given data
+        with open(args[0], 'r') as input:
+            # add range of nodes from 0 to the number of required nodes - 1
+            self.graph.add_nodes_from(range(0, int(input.readline()) - 1))
 
     # Parse single line from file            
-    def parse_line(line):
+    def add_edge_from_line(line):
         pass
+
+if __name__ == '__main__': 
+    g = Graph('g0.txt')
+    print g.graph.nodes()
