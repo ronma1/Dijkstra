a73b965c173bff20c8ae5954098a45deb9f54738
daniel <daniel4x@gmail.com>
2016-11-16 11:22:41 +0200

    Done parsing the file

--- Graph.py
+++ Graph.py
@@ -8,12 +8,21 @@ class Graph:
         # Open the input file and create the graph from the given data
         with open(args[0], 'r') as input:
             # add range of nodes from 0 to the number of required nodes - 1
-            self.graph.add_nodes_from(range(0, int(input.readline()) - 1))
+            self.nodes = int(input.readline())
+            self.edges = int(input.readline())
+            self.graph.add_nodes_from(range(0, self.nodes - 1))
+            for line in input.readlines():
+                self.add_edge_from_line(line)
 
     # Parse single line from file            
-    def add_edge_from_line(line):
-        pass
+    def add_edge_from_line(self, line):
+        parms =line.split()
+        self.graph.add_edge(int (parms[0]),int(parms[1]),weight=float(parms[2]))
 
 if __name__ == '__main__': 
     g = Graph('g0.txt')
+    print " graph: " + str(g)
     print g.graph.nodes()
+    print "\n"
+    for edge in g.graph.edges():
+        print str(edge[0]) + " " + str(edge[1]) + " " + str(g.graph.get_edge_data(edge[0], edge[1]))
