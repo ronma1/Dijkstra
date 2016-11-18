5fef4401a3304b61b99927a927c456745cdd7fd7
Daniel Alfasi <daniela@perfectomobile.com>
2016-11-18 12:19:05 +0200

    added graph_algo.py

---
+++ graph_algo.py
@@ -0,0 +1,24 @@
+
+class Graph_algo:
+
+    # loads Graph.graph instance into Graph_algo self.graph
+    def __init__(graph):
+        pass
+
+    # calculate the distance between two nodes
+    # should return float number
+    def calc_dist(v1, v2):
+        pass
+
+    # calculate the path between two nodes
+    # should return a list [v1 ..... v2] represents the path
+    def calc_path(v1, v2):
+        pass
+
+    # calculates the path between two nodes
+    # Without pass over the a blacklist of nodes 
+    # should return a list [v1 ..... v2] represents the path 
+    def calc_path_with_blacklist(v1, v2, *blacklist):
+        pass
+
+    
