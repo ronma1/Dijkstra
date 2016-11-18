ad798987ed9fe0347c90f5b2fcdb67186738b792
DESKTOP-UCTCRNV\dean <proview10@gmail.com>
2016-11-18 19:26:03 +0200

    implament  calc_dist and calc_path

--- graph_algo.py
+++ graph_algo.py
@@ -1,19 +1,20 @@
+import networkx as nx
 
 class Graph_algo:
 
     # loads Graph.graph instance into Graph_algo self.graph
-    def __init__(graph):
-        pass
+    def __init__(self,graph):
+        self.graph=graph
 
     # calculate the distance between two nodes
     # should return float number
     def calc_dist(v1, v2):
-        pass
+        return self.graph.shortest_path_length(self.graph,v1,v2)
 
     # calculate the path between two nodes
     # should return a list [v1 ..... v2] represents the path
-    def calc_path(v1, v2):
-        pass
+    def calc_path(self,v1, v2):
+       return self.graph.shortest_path(self.graph,v1,v2)
 
     # calculates the path between two nodes
     # Without pass over the a blacklist of nodes 
