2eb78b42b6f0523adffdf37691f862ea80a756fc
Ron Matsliah <ronma1@gmail.com>
2016-11-19 21:50:41 +0200

    graph algo update

--- graph_algo.py
+++ graph_algo.py
@@ -4,31 +4,48 @@ from Graph import Graph
 class Graph_algo:
 
     # loads Graph.graph instance into Graph_algo self.graph
-    def __init__(self, graph):
-        self.graph = graph
+    def __init__(self,name):
+        g = Graph(name)
+        self.graph=g.graph
 
-    # calculate the distance between two nodes
+    # Returns the shortest path length from source to target in a weighted graph using networkx function
     # should return float number
-    def calc_dist(self, v1, v2):
-        return nx.shortest_path(self.graph, source = v1, target = v2)
+    def calc_dist(self,v1, v2):
+        return nx.dijkstra_path_length(self.graph,v1,v2)
 
     # calculate the path between two nodes
-    # should return a list [v1 ..... v2] represents the path
-    def calc_path(v1, v2):
-        pass
+    # Using networkx function - should return a list [v1 ..... v2] represents the shortest path
+    def calc_path(self,v1, v2):
+       return nx.dijkstra_path(self.graph,v1,v2)
 
     # calculates the path between two nodes
     # Without pass over the a blacklist of nodes 
     # should return a list [v1 ..... v2] represents the path 
-    def calc_path_with_blacklist(self, v1, v2, *blacklist):
+    def calc_path_with_blacklist(self,v1, v2, blacklist):
+        for node in blacklist:
+            self.graph.remove_node(node)
+        return nx.dijkstra_path_length(self.graph,v1,v2)
+        
+
+    #Return the diameter of the graph G
+    def diamiter(self):
+        return nx.diameter(self.graph)
+        
+    #Return the radius of the graph G
+    def radius(self):
+        return nx.radius(self.graph)
+
+    def is_Triangle_inequality(self):
+        for i in range(0,nx.number_of_nodes(self.graph),1):
+            for j in range(0,nx.number_of_nodes(self.graph)):
+                
+                if nx.dijkstra_path_length(self.graph,i,j)>1:
+                    return False
+        return True
+
+    def time(self):
         pass
 
-# main test, should be removed !
 if __name__ == '__main__': 
-    G = Graph('g0.txt')
-    
-    #print the nodes and distance between 
-    for edge in G.graph.edges():
-        print str(edge[0]) + " " + str(edge[1]) + " " + str(G.graph.get_edge_data(edge[0], edge[1]))
-    
-    print Graph_algo(G).calc_dist(0,5)
+    g1=Graph_algo('g0.txt')
+    print g1.is_Triangle_inequality() 
