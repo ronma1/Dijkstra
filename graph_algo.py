2aa9d4771735c78a0beac38861094a248409aa93
daniel4x <daniel4x@gmail.com>
2016-11-19 15:37:35 +0200

    Added graph generator

--- graph_algo.py
+++ graph_algo.py
@@ -1,14 +1,16 @@
+import networkx as nx
+from Graph import Graph
 
 class Graph_algo:
 
     # loads Graph.graph instance into Graph_algo self.graph
-    def __init__(graph):
-        pass
+    def __init__(self, graph):
+        self.graph = graph
 
     # calculate the distance between two nodes
     # should return float number
-    def calc_dist(v1, v2):
-        pass
+    def calc_dist(self, v1, v2):
+        return nx.shortest_path(self.graph, source = v1, target = v2)
 
     # calculate the path between two nodes
     # should return a list [v1 ..... v2] represents the path
@@ -18,7 +20,15 @@ class Graph_algo:
     # calculates the path between two nodes
     # Without pass over the a blacklist of nodes 
     # should return a list [v1 ..... v2] represents the path 
-    def calc_path_with_blacklist(v1, v2, *blacklist):
+    def calc_path_with_blacklist(self, v1, v2, *blacklist):
         pass
 
+# main test, should be removed !
+if __name__ == '__main__': 
+    G = Graph('g0.txt')
     
+    #print the nodes and distance between 
+    for edge in G.graph.edges():
+        print str(edge[0]) + " " + str(edge[1]) + " " + str(G.graph.get_edge_data(edge[0], edge[1]))
+    
+    print Graph_algo(G).calc_dist(0,5)
