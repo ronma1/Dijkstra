925ab158e6c8223adebfb8ac7be822a856387f91
DESKTOP-UCTCRNV\dean <proview10@gmail.com>
2016-11-19 16:21:12 +0200

    fixing calc_dist

--- graph_algo.py
+++ graph_algo.py
@@ -8,13 +8,13 @@ class Graph_algo:
 
     # Returns the shortest path length from source to target in a weighted graph using networkx function
     # should return float number
-    def calc_dist(v1, v2):
-        return self.graph.dijkstra_path_length(self.graph,v1,v2)
+    def calc_dist(self,v1, v2):
+        return nx.dijkstra_path_length(self.graph,v1,v2)
 
     # calculate the path between two nodes
     # Using networkx function - should return a list [v1 ..... v2] represents the shortest path
     def calc_path(self,v1, v2):
-       return self.graph.shortest_path(self.graph,v1,v2)
+       return nx.shortest_path(self.graph,v1,v2)
 
     # calculates the path between two nodes
     # Without pass over the a blacklist of nodes 
